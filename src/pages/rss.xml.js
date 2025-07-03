eab938b244e454d35e3adc05db462b97c0c7c70e
Anshal543 <sherhasan543@gmail.com>
2025-07-03 23:16:20 +0500

    rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,12 @@
+import rss, { pagesGlobToRssItems }  from "@astrojs/rss";
+
+export async function GET(context) {
+    return rss({
+        title: "Anshal's Astro Blog",
+        description: "A blog about my learning journey with Astro.",
+        site: context.site,
+        items: await pagesGlobToRssItems(import.meta.glob("./posts/*.md")),
+        customData: `<language>en-us</language>`,
+    })
+}
+
