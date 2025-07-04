ebb761d01c8c2d48335593b1aba5efe6b2388a3f
Anshal543 <sherhasan543@gmail.com>
2025-07-04 22:39:34 +0500

    added zod, content, complete unit 6

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,18 @@
-import rss, { pagesGlobToRssItems }  from "@astrojs/rss";
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
 
 export async function GET(context) {
-    return rss({
-        title: "Anshal's Astro Blog",
-        description: "A blog about my learning journey with Astro.",
-        site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob("./posts/*.md")),
-        customData: `<language>en-us</language>`,
-    })
-}   
-
+  const posts = await getCollection("blog");
+  return rss({
+    title: "Anshal's Astro Blog",
+    description: "A blog about my learning journey with Astro.",
+    site: context.site,
+    items: posts.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/posts/${post.id}/`,
+    })),
+    customData: `<language>en-us</language>`,
+  });
+}
